d9110e2151f0ee472449a587c39af5d195b205b1
Bulatr <bulat_r@inbox.ru>
2023-05-28 21:27:21 +0800

    Добавил к категориям иерархию

--- project/products/models.py
+++ project/products/models.py
@@ -13,6 +13,38 @@ class Category(models.Model):
     name = models.CharField(max_length=255, null=False, unique=True)
     created_data = models.DateTimeField(auto_now_add=True)
     updatet_data = models.DateTimeField(auto_now=True)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True, related_name='children')
+
+    def __str__(self):
+        return self.name
+
+    def get_ancestors(self):
+        """
+        Метод для получения всех предков данной категории.
+        """
+        ancestors = []
+        category = self
+        while category.parent:
+            category = category.parent
+            ancestors.append(category)
+        return ancestors
+
+    def get_descendants(self):
+        """
+        Метод для получения всех потомков данной категории.
+        """
+        descendants = []
+        self._get_descendants_recursive(descendants)
+        return descendants
+
+    def _get_descendants_recursive(self, descendants):
+        """
+        Вспомогательный рекурсивный метод для получения всех потомков.
+        """
+        children = self.children.all()
+        descendants.extend(children)
+        for child in children:
+            child._get_descendants_recursive(descendants)
 
 class Product(models.Model):
     '''

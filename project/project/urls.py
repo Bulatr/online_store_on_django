c0c244049d59409055596b9a3fcb9fa36279c4f2
Bulatr <bulat_r@inbox.ru>
2023-06-06 18:45:24 +0800

    небольшое изменение urls

--- project/project/urls.py
+++ project/project/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from products.views import ProductList
 
 urlpatterns = [
     path('admin/', admin.site.urls),

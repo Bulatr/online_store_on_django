1ca7d307e22e9ad0ea1b8e2d358536ae5cd7031b
Bulatr <bulat_r@inbox.ru>
2023-06-14 22:32:04 +0800

    Наконец-то победил пагинацию

--- project/products/templatetags/custom_tags.py
+++ project/products/templatetags/custom_tags.py
@@ -5,17 +5,29 @@ from django.template.defaultfilters import stringfilter
 
 register = template.Library()
 
+# Нормально работающий тег, который заменяет параметры url
 @register.simple_tag(takes_context=True)
 def replace_query_param(context, param, value):
     request = context['request']
     query_params = request.GET.copy()
-    query_params[param] = value
+    query_params.setlist(param, [value])
     return request.path + '?' + query_params.urlencode()
 
-@register.filter
-@stringfilter
-def add_query_param(url, param_name, param_value):
-    params = {param_name: param_value}
+# на всякий случай оставил, некорректно работает
+@register.simple_tag(takes_context=True)
+def add_query_param_pagination(context, url):
+    request = context['request']
+    params = request.GET.copy()
+    params.update(request.POST)  # Если используете и POST параметры
+    # Исключаем параметр 'page' из словаря params
+    # params.pop('page', None)
     query_string = urlencode(params)
-    separator = '&' if '?' in url else '?'
-    return f'{url}{separator}{query_string}'
+    return f'{url}?{query_string}'
+
+# не помню что это
+@register.simple_tag(takes_context=True)
+def url_replace(context, **kwargs):
+   d = context['request'].GET.copy()
+   for k, v in kwargs.items():
+       d[k] = v
+   return d.urlencode()
